c121d1cab943b1110e7ea83b3087c4fe44edc92f
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2018-06-09 23:42:30 -0300

    assorted changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './app';
+
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+
+import App from 'src/app';
+import registerServiceWorker from 'src/registerServiceWorker';
 
 ReactDOM.render(
   <App />,
