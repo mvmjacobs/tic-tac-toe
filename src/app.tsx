26602d50f79c8665b41fa74efb3722763adffffd
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2018-06-09 22:49:05 -0300

    does this work

--- src/app.tsx
+++ src/app.tsx
@@ -9,11 +9,11 @@ class App extends React.Component {
 			<div className="app">
 				<header className="app-header">
 					<img src={logo} className="app-logo" alt="logo" />
-					<h1 className="app-title">Welcome to React</h1>
+					<h1 className="app-title">Welcome to React Tic-Tac-Toe</h1>
 				</header>
 				<p className="app-intro">
-					To get started, edit <code>src/app.tsx</code> and save to reload.
-        </p>
+					The best game you'll ever play today!
+				</p>
 			</div>
 		);
 	}
