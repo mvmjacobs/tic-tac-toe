f605e4d670fdbe27e5913d38e72ed72c3a9e7e6c
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2018-06-12 20:33:59 -0300

    TODO: Fix later

--- src/game/board/board.tsx
+++ src/game/board/board.tsx
@@ -8,25 +8,9 @@ import 'src/game/square/square.css';
 import Square from 'src/game/square/square';
 
 export default class Board extends React.Component<any, any> {
-	constructor(props: any) {
-		super(props);
-		this.state = {
-			squares: Array(9).fill(null),
-			xIsNext: true
-		};
-	}
-
 	public render() {
-		const winner = this.calculateWinner(this.state.squares);
-		let status: string;
-		if (winner)
-			status = `Winner: ${winner}`;
-		else
-			status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-
 		return (
 			<div>
-				<div className="status">{status}</div>
 				<div className="board-row">
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
@@ -47,38 +31,6 @@ export default class Board extends React.Component<any, any> {
 	}
 
 	private renderSquare(i: number) {
-		return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
-	}
-
-	private handleClick(i: number) {
-		const squares = this.state.squares.slice();
-		if (this.calculateWinner(squares) || squares[i])
-			return;
-
-		squares[i] = this.state.xIsNext ? 'X' : 'O';
-		this.setState({
-			squares,
-			xIsNext: !this.state.xIsNext
-		});
-	}
-
-	private calculateWinner(squares: any[]) {
-		const lines = [
-			[0, 1, 2],
-			[3, 4, 5],
-			[6, 7, 8],
-			[0, 3, 6],
-			[1, 4, 7],
-			[2, 5, 8],
-			[0, 4, 8],
-			[2, 4, 6],
-		];
-		for (const line of lines) {
-			const [a, b, c] = line;
-			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-				return squares[a];
-			}
-		}
-		return null;
+		return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
 	}
 }
--- src/game/game.tsx
+++ src/game/game.tsx
@@ -6,18 +6,74 @@ import 'src/game/game.css';
 // components
 import Board from 'src/game/board/board';
 
-export default class Game extends React.Component {
+export default class Game extends React.Component<any, any> {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			history: [
+				{ squares: Array(9).fill(null) }
+			],
+			xIsNext: true
+		};
+	}
+
 	public render() {
+		const history = this.state.history;
+		const current = history[history.length - 1];
+		const winner = this.calculateWinner(current.squares);
+
+		let status: string;
+		if (winner)
+			status = `Winner: ${winner}`;
+		else
+			status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+
 		return (
 			<div className="game">
 				<div className="game-board">
-					<Board />
+					<Board squares={current.squares} onClick={(i: number) => this.handleClick(i)} />
 				</div>
 				<div className="game-info">
-					<div>{/* status */}</div>
+					<div>{status}</div>
 					<ol>{/* TODO */}</ol>
 				</div>
 			</div>
 		);
+	}
+
+	private handleClick(i: number) {
+		const history = this.state.history;
+		const current = history[history.length - 1];
+		const squares = current.squares.slice();
+		if (this.calculateWinner(squares) || squares[i])
+			return;
+
+		squares[i] = this.state.xIsNext ? 'X' : 'O';
+		this.setState({
+			history: history.concat([{
+				squares
+			}]),
+			xIsNext: !this.state.xIsNext
+		});
+	}
+
+	private calculateWinner(squares: any[]) {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+		for (const line of lines) {
+			const [a, b, c] = line;
+			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+				return squares[a];
+			}
+		}
+		return null;
 	}
 }
